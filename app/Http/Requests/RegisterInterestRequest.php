8e23f35162fb397479ab19b83862fede9789078c
Rati Tsiklauri <ratiwiko@gmail.com>
2026-05-18 23:10:29 +0400

    update

--- app/Http/Requests/RegisterInterestRequest.php
+++ app/Http/Requests/RegisterInterestRequest.php
@@ -20,4 +20,24 @@ class RegisterInterestRequest extends FormRequest
             'package' => 'required|in:supporter,friend,partner,cofounder',
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'company_name.required' => 'კომპანიის სახელი სავალდებულოა.',
+            'company_name.string'   => 'კომპანიის სახელი უნდა იყოს ტექსტი.',
+            'company_name.max'      => 'კომპანიის სახელი არ უნდა აღემატებოდეს 255 სიმბოლოს.',
+
+            'contact_person.required' => 'საკონტაქტო პირის სახელი სავალდებულოა.',
+            'contact_person.string'   => 'საკონტაქტო პირის სახელი უნდა იყოს ტექსტი.',
+            'contact_person.max'      => 'საკონტაქტო პირის სახელი არ უნდა აღემატებოდეს 255 სიმბოლოს.',
+
+            'phone.required' => 'ტელეფონის ნომერი სავალდებულოა.',
+            'phone.string'   => 'ტელეფონის ნომერი უნდა იყოს ტექსტი.',
+            'phone.max'      => 'ტელეფონის ნომერი არ უნდა აღემატებოდეს 20 სიმბოლოს.',
+
+            'package.required' => 'პაკეტის არჩევა სავალდებულოა.',
+            'package.in'       => 'არჩეული პაკეტი არასწორია. დასაშვები მნიშვნელობებია: supporter, friend, partner, cofounder.',
+        ];
+    }
 }
